91b0ff2099441b51c33a287faea8b512efb6cc60
Bar Barina <itsbarbarina@gmail.com>
2023-02-26 15:39:47 +0200

    Basic files setup

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
 const { createApp } = Vue
 
-// import AboutPage from './pages/AboutPage.js'
+import AppHeader from './cmps/AppHeader.js'
+import AppFooter from './cmps/AppFooter.js'
+
+import HomePage from './pages/HomePage.js'
+import AboutPage from './pages/AboutPage.js'
 
 const options = {
-    template: `
+  template: `
         <section class="container">
-           
+        <AppHeader @setRoute="route = $event"/>
+        <main class="router-view">
+                <HomePage v-if="route === 'HomePage'"/>
+                <!-- <CarIndex v-if="route === 'CarIndex'"/> -->
+                <AboutPage v-if="route === 'AboutPage'"/>
+            </main>
         </section>
     `,
-    data() {
-        return {
-            
-        }
-    },
-    components: {
-    
+  data() {
+    return {
+      route: 'HomePage',
     }
+  },
+  components: {
+    AppHeader,
+    AppFooter,
+    HomePage,
+    AboutPage,
+  },
 }
-
 
 const app = createApp(options)
 app.mount('#app')
