ccb6ee58e344512f854e2bc86bcd83b691d08c66
Bar Barina <itsbarbarina@gmail.com>
2023-02-26 15:12:10 +0200

    Miss Book Start

---
+++ app.js
@@ -0,0 +1,23 @@
+const { createApp } = Vue
+
+// import AboutPage from './pages/AboutPage.js'
+
+const options = {
+    template: `
+        <section class="container">
+           
+        </section>
+    `,
+    data() {
+        return {
+            
+        }
+    },
+    components: {
+    
+    }
+}
+
+
+const app = createApp(options)
+app.mount('#app')
